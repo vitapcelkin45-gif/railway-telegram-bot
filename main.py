509d4f085258801491a6c6f8e7f46ff52423574b
vitapcelkin45-gif <vitapcelkin45@gmail.com>
2026-01-09 17:37:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 import os
 from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
+# Загружаем переменные из .env
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
+# Команда /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Я жив.")
+    await update.message.reply_text("Привет! Я бот.")
 
-if __name__ == '__main__':
+# Команда /help
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Напиши /start чтобы начать.")
+
+# Запуск
+if __name__ == "__main__":
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("help", help_command))
     app.run_polling()
